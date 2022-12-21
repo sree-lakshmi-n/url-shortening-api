e0d1fb9703b59f3f3a315dc5a6b267d0173f68ef
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-21 19:28:06 +0530

    Header Component. Nav component renamed to TopNav.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Nav from "./components/Nav/Nav";
+import TopNav from "./components/Nav/TopNav";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <TopNav />
+      <Header />
     </div>
   );
 }
