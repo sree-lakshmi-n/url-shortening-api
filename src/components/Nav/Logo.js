48603f611aa717d856905af3b35a7b4449eef276
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-21 19:38:04 +0530

    Logo component

---
+++ src/components/Nav/Logo.js
@@ -0,0 +1,9 @@
+import logo from "../../images/logo.svg";
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <img src={logo} alt="Shortly logo" />
+    </div>
+  );
+};
+export default Logo;
